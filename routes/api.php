fe418f669c6794977dd949ad90891869223f93eb
parbhez <masud.affb@gmail.com>
2022-09-13 01:40:09 +0600

    Laravel API Call and data insert and view into html by the vue js watch property

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\Api\UserController;
+use App\Http\Controllers\PostController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +19,7 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/add-user',[UserController::class,'addUser'])->name('add-user');
+Route::post('/save-user',[UserController::class,'saveUser'])->name('save-user');
+Route::get('/view-user',[UserController::class,'viewUser'])->name('view-user');
